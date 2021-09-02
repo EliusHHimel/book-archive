664ecfec64ada58484ece59946e71aa01c94850a
Hm Elius Hossain Himel <himelahmed201617@gmail.com>
2021-09-02 21:43:22 +0600

    project complete

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // Search Button Click Handler
 
-document.getElementById('search-button').addEventListener('click', function () {
+const getSearchResults = () => {
 
     const searchText = document.getElementById('search-field').value;
 
@@ -10,7 +10,7 @@ document.getElementById('search-button').addEventListener('click', function () {
     fetch(`https://openlibrary.org/search.json?q=${searchText}`)
         .then(Response => Response.json())
         .then(data => displayBook(data))
-})
+}
 
 const toggleSpinner = (displayStyle, resultDisplayStyle) => {
     document.getElementById('spinner').style.display = displayStyle;
@@ -18,31 +18,37 @@ const toggleSpinner = (displayStyle, resultDisplayStyle) => {
 }
 
 // Display Books in Search Result
-function displayBook(books) {
+const displayBook = books => {
     const resultContainer = document.getElementById('result-container');
     resultContainer.textContent = ''; //clear the previous results
-    books = books.docs;
+
+    const booksInformation = books.docs;
     console.log(books) //temporary console view
 
-    for (const book of books) {
+    // Total Found Results
+    const totalCount = document.createElement('h4');
+    totalCount.innerText = `Total Books found: ${booksInformation.length}`;
+    resultContainer.appendChild(totalCount);
+
+    // Show books information
+    booksInformation.forEach(book => {
         const column = document.createElement('div');
         column.classList.add('col-3', 'g-4', 'd-flex', 'justify-content-center')
         // console.log(book.title)
         column.innerHTML = `
-        <div class='card' style="width: 18rem;">
-        <img src="${book.cover_i ? 'https://covers.openlibrary.org/b/id/' + book.cover_i + '-M.jpg' : 'https://www.forewordreviews.com/books/covers/networking-for-people-who-hate-networking.jpg'}" class="card-img-top" alt="">
-        <div class="card-body">
-                <h5 class="card-title">${book.title}</h5>
+            <div class='card' style="width: 18rem;">
+            <img src="${book.cover_i ? 'https://covers.openlibrary.org/b/id/' + book.cover_i + '-M.jpg' : 'https://www.forewordreviews.com/books/covers/networking-for-people-who-hate-networking.jpg'}" class="card-img-top" alt="">
+            <div class="card-body">
+                    <h5 class="card-title">${book.title}</h5>
+                </div>
+                <ul class="list-group list-group-flush">
+                <li class="list-group-item">Author: ${book.author_name ? book.author_name[0] : 'Author name not found'}</li >
+                <li class="list-group-item">First Published: ${book.first_publish_year ? book.first_publish_year : 'First publish year not found'}</li >
+            <li class="list-group-item">A third item</li>
+            </ul>
             </div>
-            <ul class="list-group list-group-flush">
-            <li class="list-group-item">Author: ${book.author_name ? book.author_name[0] : 'Author name not found'}</li >
-            <li class="list-group-item">First Published: ${book.first_publish_year ? book.first_publish_year : 'First publish year not found'}</li >
-        <li class="list-group-item">A third item</li>
-        </ul>
-        </div>
-        `;
+            `;
         resultContainer.appendChild(column);
         toggleSpinner('none', 'flex');
-    }
-
+    })
 }
