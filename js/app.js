b124f188d56212b5aba5f659f7f19187572dfe93
Hm Elius Hossain Himel <himelahmed201617@gmail.com>
2021-09-02 22:20:47 +0600

    Indent fix

--- js/app.js
+++ js/app.js
@@ -47,12 +47,12 @@ const displayBook = books => {
             <div class='card' style="width: 18rem;">
             <img src="${book.cover_i ? 'https://covers.openlibrary.org/b/id/' + book.cover_i + '-M.jpg' : 'https://www.forewordreviews.com/books/covers/networking-for-people-who-hate-networking.jpg'}" class="card-img-top" alt="">
             <div class="card-body">
-                    <h5 class="card-title">${book.title}</h5>
+                <h5 class="card-title">${book.title}</h5>
                 </div>
                 <ul class="list-group list-group-flush">
                 <li class="list-group-item">Author: ${book.author_name ? book.author_name[0] : 'Author name not found'}</li >
                 <li class="list-group-item">First Published: ${book.first_publish_year ? book.first_publish_year : 'First publish year not found'}</li >
-            <li class="list-group-item">Publisher: ${book.publisher ? book.publisher[0] : 'Publisher not found'}</li>
+                <li class="list-group-item">Publisher: ${book.publisher ? book.publisher[0] : 'Publisher not found'}</li>
             </ul>
             </div>
             `;
