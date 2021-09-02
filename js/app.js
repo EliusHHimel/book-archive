b696b19d6c5f6881c7207697f1e7becf127896ff
Hm Elius Hossain Himel <himelahmed201617@gmail.com>
2021-09-02 19:40:53 +0600

    almost complete

--- js/app.js
+++ js/app.js
@@ -4,20 +4,29 @@ document.getElementById('search-button').addEventListener('click', function () {
 
     const searchText = document.getElementById('search-field').value;
 
+    document.getElementById('search-field').value = ''; //clear search field
+    toggleSpinner('block', 'none') //show the toggle spinner
+
     fetch(`https://openlibrary.org/search.json?q=${searchText}`)
         .then(Response => Response.json())
         .then(data => displayBook(data))
 })
 
+const toggleSpinner = (displayStyle, resultDisplayStyle) => {
+    document.getElementById('spinner').style.display = displayStyle;
+    document.getElementById('result-container').style.display = resultDisplayStyle;
+}
+
 // Display Books in Search Result
 function displayBook(books) {
     const resultContainer = document.getElementById('result-container');
+    resultContainer.textContent = ''; //clear the previous results
     books = books.docs;
-    console.log(books)
+    console.log(books) //temporary console view
 
     for (const book of books) {
         const column = document.createElement('div');
-        column.classList.add('col-4', 'g-4')
+        column.classList.add('col-3', 'g-4', 'd-flex', 'justify-content-center')
         // console.log(book.title)
         column.innerHTML = `
         <div class='card' style="width: 18rem;">
@@ -26,12 +35,14 @@ function displayBook(books) {
                 <h5 class="card-title">${book.title}</h5>
             </div>
             <ul class="list-group list-group-flush">
-            <li class="list-group-item">Author: ${book.author_name[0]}</li >
-            <li class="list-group-item">First Published: ${book.first_publish_year}</li >
+            <li class="list-group-item">Author: ${book.author_name[0] ? book.author_name[0] : 'Author name not found'}</li >
+            <li class="list-group-item">First Published: ${book.first_publish_year ? book.first_publish_year : 'First publish year not found'}</li >
         <li class="list-group-item">A third item</li>
         </ul>
         </div>
         `;
-        resultContainer.appendChild(column)
+        resultContainer.appendChild(column);
+        toggleSpinner('none', 'flex');
     }
+
 }
