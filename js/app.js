fe69464dab347e7ec4202fc6e396a11d11476d0a
Hm Elius Hossain Himel <himelahmed201617@gmail.com>
2021-09-02 20:32:48 +0600

    bug fixed

--- js/app.js
+++ js/app.js
@@ -30,12 +30,12 @@ function displayBook(books) {
         // console.log(book.title)
         column.innerHTML = `
         <div class='card' style="width: 18rem;">
-        <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="">
+        <img src="${book.cover_i ? 'https://covers.openlibrary.org/b/id/' + book.cover_i + '-M.jpg' : 'https://www.forewordreviews.com/books/covers/networking-for-people-who-hate-networking.jpg'}" class="card-img-top" alt="">
         <div class="card-body">
                 <h5 class="card-title">${book.title}</h5>
             </div>
             <ul class="list-group list-group-flush">
-            <li class="list-group-item">Author: ${book.author_name[0] ? book.author_name[0] : 'Author name not found'}</li >
+            <li class="list-group-item">Author: ${book.author_name ? book.author_name[0] : 'Author name not found'}</li >
             <li class="list-group-item">First Published: ${book.first_publish_year ? book.first_publish_year : 'First publish year not found'}</li >
         <li class="list-group-item">A third item</li>
         </ul>
